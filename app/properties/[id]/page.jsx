bf8ed2130315a6081a28f7916184719f6e6edb37
Eric Ross <epr@rossmbox.com>
2025-08-09 12:47:59 -0700

    Full navigation hook demo

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,17 @@
 'use client';
-import { useRouter } from "next/navigation";
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
 
 const PropertyPage = () => {
   const router = useRouter();
-  console.log('Hello')
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name');
+  const pathname = usePathname();
   return (
     <div>
-      <button onClick={ () => router.push('/') } className="bg-blue-500 p2">Go Home</button>
+      <button onClick={ () => router.push('/') } className="bg-blue-500 p2">
+        Go Home { id } { name } from { pathname }
+      </button>
     </div>
   )
 }
