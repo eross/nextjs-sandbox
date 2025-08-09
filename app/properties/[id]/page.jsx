e98b3309e023e0227114b3a1c061ebd5171fd56d
Eric Ross <epr@rossmbox.com>
2025-08-09 12:36:45 -0700

    client component with button

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+'use client';
+import { useRouter } from "next/navigation";
 
 const PropertyPage = () => {
+  const router = useRouter();
+  console.log('Hello')
   return (
-    <div>PropertyPage</div>
+    <div>
+      <button onClick={ () => router.push('/') } className="bg-blue-500 p2">Go Home</button>
+    </div>
   )
 }
 
-export default PropertyPage
+export default PropertyPage;
