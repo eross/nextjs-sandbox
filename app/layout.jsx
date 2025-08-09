ed43022074eada6f8976121eb0da2a296a9b7299
Eric Ross <epr@rossmbox.com>
2025-08-09 14:26:38 -0700

    Added scaffold navbar component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import '@/assets/styles/globals.css'
+import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
@@ -9,7 +10,8 @@ export const metadata = {
 const MainLayout = ({children}) => {
   return (<html lang='en'>
     <body>
-        <div>{children}</div>
+      <Navbar/>
+        <main>{children}</main>
     </body>
     </html>
   )
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,17 +1,8 @@
 'use client';
-import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
-
 const PropertyPage = () => {
-  const router = useRouter();
-  const { id } = useParams();
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name');
-  const pathname = usePathname();
   return (
     <div>
-      <button onClick={ () => router.push('/') } className="bg-blue-500 p2">
-        Go Home { id } { name } from { pathname }
-      </button>
+        PropertyPage
     </div>
   )
 }
