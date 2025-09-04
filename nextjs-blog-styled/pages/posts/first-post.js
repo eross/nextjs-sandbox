4083c1fea069a6f4a5208010ba75cf5943d8bf0e
Eric Ross <epr@rossmbox.com>
2025-09-03 19:37:07 -0700

    Add header data and use 3rd party scripts

--- nextjs-blog-styled/pages/posts/first-post.js
+++ nextjs-blog-styled/pages/posts/first-post.js
@@ -1,8 +1,19 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log('script loaded.  window.FB has been populated')}
+      >
+      </Script>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
