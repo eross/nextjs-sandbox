95f09fff08acac0c4b5f93002ab746de00bb5c81
Eric Ross <epr@rossmbox.com>
2025-10-16 20:05:13 -0700

    moved store to index.ts

--- desktop/redux-movie-store/pages/index.tsx
+++ desktop/redux-movie-store/pages/index.tsx
@@ -1,7 +1,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import {createStore} from 'redux';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,37 +12,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-const initialState = {
-  movies: [],
-  basket: [],
-  likedMovies: []
-}
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'ADD_MOVIE':
-      return {
-        ...state,
-        movies: [...state.movies, action.payload]
-      };
-    case 'ADD_TO_BASKET':
-      return {
-        ...state,
-        basket: [...state.basket, action.payload]
-      };
-    case 'ADD_TO_LIKED_MOVIES':
-      return {
-        ...state,
-        likedMovies: [...state.likedMovies, action.payload]
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-export default store;
 
 export default function Home() {
   return (
